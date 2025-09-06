034d32d8a0fd810cbf4bff560f02541466af8722
Elvir <esandovalp5@miumg.edu.gt>
2025-09-06 10:04:13 -0600

    MVC Capacitacion

---
+++ CapaVista/Navegador_Prueba.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaVista
+{
+    public partial class Navegador_Prueba : Form
+    {
+        public Navegador_Prueba()
+        {
+            InitializeComponent();
+        }
+
+        private void navegador1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
